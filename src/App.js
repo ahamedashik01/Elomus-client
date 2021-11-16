ab02116d7a656bd5520328892ee6d1d325f731ae
ahamedashik01 <ashikshamim.007@gmail.com>
2021-11-17 00:01:18 +0600

    firebase config authcontext private route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './contexts/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <Router>
+
+          <Switch>
+            <Route exact path="/">
+
+            </Route>
+            <Route path="*">
+
+            </Route>
+          </Switch>
+        </Router>
+
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            THis is log in
+        </div>
+    );
+};
+
+export default Login;
